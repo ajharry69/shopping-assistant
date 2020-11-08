38c765c7b2cd238bd8243965e306603de303ac65
Orinda Harrison <ajuongharry35@gmail.com>
2020-11-08 10:22:12 +0300

    Refactor method name and add 'flowOn'

--- data/source/common/src/main/java/com/xently/data/source/common/Utils.kt
+++ data/source/common/src/main/java/com/xently/data/source/common/Utils.kt
@@ -4,8 +4,13 @@ import com.xently.common.data.TaskResult
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
-fun <T> Flow<TaskResult<T>>.withLoading(): Flow<TaskResult<T>> = flow {
+fun <T> Flow<TaskResult<T>>.whileLoading(
+    flowOn: CoroutineContext = EmptyCoroutineContext,
+): Flow<TaskResult<T>> = flow {
     emit(TaskResult.Loading)
-    emitAll(this@withLoading)
-}
+    emitAll(this@whileLoading)
+}.flowOn(flowOn)
